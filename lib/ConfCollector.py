84409b30b2b8eb3fde1f8b3027a888c0dc04b1e7
ibehii <behii@tutanota.com>
2024-05-03 18:43:22 +0330

    fixing the error that is acquired when there are fewer links than demands

--- lib/ConfCollector.py
+++ lib/ConfCollector.py
@@ -21,10 +21,10 @@ def VmessCollector(amount: int) -> list[str]:
     
     ServersLink: list[str] = []
     
+    MaxAvailableLink: int = len(ConfigUrlData)
+    
     # ======== # encode the vmess link and extract its info # ======== #
     for index, Link in enumerate(ConfigUrlData):
-        if index == amount:
-            return ServersLink
         
         # ======== # Opening the link and change it # ======== #
         Link: str = Link.replace('vmess://', '')#type: ignore
@@ -35,9 +35,15 @@ def VmessCollector(amount: int) -> list[str]:
         # ======== # after editing the link, encode it again and return it # ======== #
         ServersLink.append('vmess://' + Link ) # type: ignore
 
+        if(index == amount):
+            return ServersLink
+        elif(index == MaxAvailableLink):
+            print(f'Your demand was {amount}, but we only had {MaxAvailableLink} link')
+            return ServersLink
+        
 def VlessCollector(amount) -> list[str]:
     _API_URL: str = "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/vless"
-    ConfigUrlData: list = get('').text.split('\n')
+    ConfigUrlData: list = get(_API_URL).text.split('\n')
     
     # ======== # Deleting first 6 line that are not config # ======== #
     for _ in range(6):
@@ -45,16 +51,22 @@ def VlessCollector(amount) -> list[str]:
         
     ServersLink: list[str] = []
     
+    MaxAvailableLink: int = len(ConfigUrlData)
+    
     # ======== # encode the vmess link and extract its info # ======== #
     for index, Link in enumerate(ConfigUrlData):
-        if index == amount:
-            return ServersLink
         
         Link: list[str] = list(Link.partition('#'))
         Link[-1] = "vless Server/T.me/BZHNAM"
                 
         # ======== # after editing the link, encode it again and return it # ======== #
-        ServersLink.append(''.join(encoded_value)) # type: ignore
+        ServersLink.append(''.join(Link)) # type: ignore
+        
+        if(index == amount):
+            return ServersLink
+        elif(index == MaxAvailableLink):
+            print(f'Your demand was {amount}, but we only had {MaxAvailableLink} link')
+            return ServersLink
 
 def TrojanCollector(amount) -> list[str]:
     _API_URL: str = "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/trojan"
@@ -66,16 +78,21 @@ def TrojanCollector(amount) -> list[str]:
         
     ServersLink: list[str] = []
     
+    MaxAvailableLink: int = len(ConfigUrlData)
+    
     # ======== # loop throw it and return # ======== #
     for index, Link in enumerate(ConfigUrlData):
-        ServersLink.append(Link)
-        if index == amount:
-            return ServersLink
         
-    # ======== # Opening the link and change it # ======== #
-    Link: str = base64.b64decode(Link.replace('trojan://', '')#type: ignore
-    Link: dict = json.loads(Link) #type: ignore
-    Link["ps"] = "Trojan Server/T.me/BZHNAM"
-    Link: str = json.dumps(Link).replace("'", '"')
+        # ======== # Opening the link and change it # ======== #
+        Link: str = Link.replace('trojan://', '')#type: ignore
+        Link: dict = json.loads(Link) #type: ignore
+        Link["ps"] = "Trojan Server/T.me/BZHNAM"
+        Link: str = json.dumps(Link).replace("'", '"')
+        
+        ServersLink.append('trojan://' + Link) # type: ignore
     
-    ServersLink.append('trojan://' + Link) # type: ignore
+        if(index == amount):
+            return ServersLink
+        elif(index == MaxAvailableLink):
+            print(f'Your demand was {amount}, but we only had {MaxAvailableLink} link')
+            return ServersLink
